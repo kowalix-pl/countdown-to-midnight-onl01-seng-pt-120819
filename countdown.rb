bf0b451b4c91a709f27bab8ea08b1ab23654b5fb
kowalix007 L.K <kowalix007@gmail.com>
2020-02-26 14:04:05 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,20 +1,9 @@
 #write your code here
 
 def countdown (x)
- x = 10
-while x >= 1  
- puts "#{x} SECOND(S)!"
- x -= 1
-end
- return "HAPPY NEW YEAR!"
+
 end
 
 def countdown_with_sleep (x)
- x = 12
-while x >= 1  
- puts "#{x} SECOND(S)!"
- x -= 1
- sleep 1
-end
- return "HAPPY NEW YEAR!"
+ 
 end
